7f98784a7071c4292ad525624d93062d373b5537
javier combita tellez <javiercombita@gmail.com>
2025-11-12 18:40:26 -0500

    Feat: se implementaron las configuraciones para el deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/VisualTokenComparator/', // Asegúrate de poner el nombre correcto del subdirectorio
   plugins: [
     tailwindcss(),
   ],
